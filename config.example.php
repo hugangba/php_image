1221f06a5c0bb417fa0724175f3c219198e5f2d6
hugangba <47004704+hugangba@users.noreply.github.com>
2025-04-19 01:01:10 +0800

    Create config.example.php

---
+++ config.example.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'GITHUB_TOKEN' => 'your_github_personal_access_token',
+    'GITHUB_REPO' => 'your_repository_name',
+    'GITHUB_OWNER' => 'your_github_username',
+    'GITHUB_BRANCH' => 'main',
+    'CUSTOM_DOMAIN' => 'https://your-custom-domain.com',
+    'CLOUDFLARE_TURNSTILE_SITE_KEY' => 'your_turnstile_site_key',
+    'CLOUDFLARE_TURNSTILE_SECRET_KEY' => 'your_turnstile_secret_key'
+];
+?>
